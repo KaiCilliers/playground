d006d4b4e8ec92285881d95e73a33b8e3c58b36a
Kai Cilliers <kaijohan428@gmail.com>
2020-10-30 21:04:34 +0000

    Notification, receiver, service

---
+++ app/src/main/java/com/example/playground/DummyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.playground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DummyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dummy)
+    }
+}
