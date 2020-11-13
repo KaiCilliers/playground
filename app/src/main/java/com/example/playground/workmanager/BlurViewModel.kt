bfad0b24e6f7c13789736d0126251589c702a826
Kai Cilliers <kaijohan428@gmail.com>
2020-11-13 14:56:08 +0000

    blur any image from phone, pass Data objects to Worker results

--- app/src/main/java/com/example/playground/workmanager/BlurViewModel.kt
+++ app/src/main/java/com/example/playground/workmanager/BlurViewModel.kt
@@ -3,9 +3,10 @@ package com.example.playground.workmanager
 import android.app.Application
 import android.net.Uri
 import androidx.lifecycle.AndroidViewModel
-import androidx.work.OneTimeWorkRequest
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
-import com.example.playground.workmanager.work.BlurWorker
+import com.example.playground.workmanager.util.KEY_IMAGE_URI
 
 class BlurViewModel (application: Application) : AndroidViewModel(application) {
 
@@ -20,9 +21,10 @@ class BlurViewModel (application: Application) : AndroidViewModel(application) {
      * @param blurLevel The amount to blur the image
      */
     internal fun applyBlur(blurLevel: Int) {
-        workManager.enqueue(
-            OneTimeWorkRequest.from(BlurWorker::class.java)
-        )
+        val blurRequest = OneTimeWorkRequestBuilder<BlurWorker>()
+            .setInputData(createInputDataForUri())
+            .build()
+        workManager.enqueue(blurRequest)
     }
 
     private fun uriOrNull(uriString: String?): Uri? {
@@ -32,6 +34,20 @@ class BlurViewModel (application: Application) : AndroidViewModel(application) {
             null
         }
     }
+
+    /**
+     * Creates the input data bundle which includes the Uri
+     * to operate on
+     * @return Data which contains the Image Uri as a String
+     */
+    private fun createInputDataForUri(): Data {
+        val data = Data.Builder()
+        imageUri?.let {
+            data.putString(KEY_IMAGE_URI, "$imageUri")
+        }
+        return data.build()
+    }
+
     /**
      * Setters
      */
--- app/src/main/java/com/example/playground/workmanager/BlurWorker.kt
+++ app/src/main/java/com/example/playground/workmanager/BlurWorker.kt
@@ -1,23 +1,37 @@
-package com.example.playground.workmanager.work
+package com.example.playground.workmanager
 
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
+import android.text.TextUtils
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import androidx.work.workDataOf
 import com.example.playground.R
+import com.example.playground.workmanager.util.KEY_IMAGE_URI
 import com.example.playground.workmanager.util.blurBitmap
 import com.example.playground.workmanager.util.makeStatusNotification
 import com.example.playground.workmanager.util.writeBitmapToFile
 import timber.log.Timber
+import java.lang.IllegalArgumentException
 
 class BlurWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
     override fun doWork(): Result {
         makeStatusNotification("Blurring image", applicationContext)
+
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
+
         return try {
-            val picture = BitmapFactory.decodeResource(
-                applicationContext.resources,
-                R.drawable.test
+            if (TextUtils.isEmpty(resourceUri)) {
+                Timber.e("Invalid input uri")
+                throw IllegalArgumentException("Invalid input uri")
+            }
+
+            val resolver = applicationContext.contentResolver
+
+            val picture = BitmapFactory.decodeStream(
+                resolver.openInputStream(Uri.parse(resourceUri))
             )
 
             val output = blurBitmap(picture, applicationContext)
@@ -27,7 +41,9 @@ class BlurWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
 
             makeStatusNotification("Output is $outputUri", applicationContext)
 
-            Result.success()
+            val outputData = workDataOf(KEY_IMAGE_URI to "$outputUri")
+
+            Result.success(outputData)
         } catch (throwable: Throwable) {
             Timber.e(throwable, "Error applying blur")
             Result.failure()
